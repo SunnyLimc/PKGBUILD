7368b78fdc15c9182f85325e71f166594de272c3
Limc <i@limc.me>
2022-11-11 13:38:13 +0800

    fix(pkg): correct PKGBUILD installation and service reworking on boot

--- clash-tuner/clash-tuner.sh
+++ clash-tuner/clash-tuner.sh
@@ -7,7 +7,7 @@ main() {
   # if networkmanager and dnsmasq exists, apply dns fix, and then refresh dns
   local dnsfix='false'
   local dnsconf='/etc/NetworkManager/dnsmasq.d/clash_tuner_dnsfix_dns.conf'
-  systemctl is-active --quiet NetworkManager && [[ ! -f $dnsconf ]] && dnsfix='true'
+  systemctl is-active --quiet NetworkManager && ps -ef | grep -E 'dnsmasq.*Net' | grep -v grep > /dev/null && [[ ! -f $dnsconf ]] && dnsfix='true' || echo "DNS will not be flushed"
   [[ $dnsfix = true ]] && echo 'server=223.5.5.5' > $dnsconf
   [[ $dnsfix = true ]] && kill -HUP $(ps -ef | grep -E 'dnsmasq.*Net' | grep -v grep | awk '{print $2}') 
   # main
@@ -16,18 +16,16 @@ main() {
   conf="$(eval echo "~$user")/.config/clash/$conf"
   # remove duplicated tailing slashes
   conf=${conf%//*}
-  [[ -s $pidpath ]] && ps -p "$(cat $pidpath)" > /dev/null && (kill $(cat $pidpath) && rm $pidpath || (echo "Can not kill services or remove pid file, you should rm or kill it manually" && return 1))
-  local sername="${backend}-tuner@${user}"
-  echo $sername
+  [[ -s $pidpath ]] && ps "$(cat $pidpath)" | grep 'clash' > /dev/null && (kill $(cat $pidpath) && rm $pidpath || (echo "Can not kill services or remove pid file, you should rm or kill it manually" && return 1))
+  local sername
+  [[ $perm = true ]] && sername="${backend}-tuner-perm@${user}" || sername="${backend}-tuner@${user}"
   echo "conf=$conf" > /usr/lib/clash-tuner/env 
-  if [[ $perm = true ]]; then
-    systemctl start "$sername"
-    echo $(systemctl show -p MainPID --value "$sername") > "$pidpath"
-  else
-    runuser -u "$user" -- systemctl --user start "$sername"
-    runuser -u "$user" -- systemctl --user show -p MainPID --value "$sername" > "$pidpath"
-  fi
+  # runuser -u "$user" -- systemctl --user start "$sername"
+  # runuser -u "$user" -- systemctl --user show -p MainPID --value "$sername" > "$pidpath"
+  systemctl start "$sername"
+  echo $(systemctl show -p MainPID --value "$sername") > "$pidpath"
   [[ $dnsfix = true ]] && rm $dnsconf && kill -HUP $(ps -ef | grep -E 'dnsmasq.*Net' | grep -v grep | awk '{print $2}') && echo "DNS cache have been flushed"
+  return 0
 }
 
 # 1=user 2=conf 3=perm 4=backend
